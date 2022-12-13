8474bcfa64e4e3da7ebe40ec467bf3a0899fb718
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2022-12-13 12:49:17 +0100

    add zoom on hover

--- src/lib/Scene.svelte
+++ src/lib/Scene.svelte
@@ -1,7 +1,25 @@
 <script>
   import { T, OrbitControls } from '@threlte/core'
-
   import { GLTF, Float, ContactShadows, Environment, HTML, Text } from '@threlte/extras';
+
+  import { tweened } from 'svelte/motion';
+  import { cubicInOut } from 'svelte/easing';
+
+
+  let cameraX = tweened(-4, {
+		duration: 2000,
+        easing: cubicInOut
+	});
+
+  let cameraY = tweened(4, {
+		duration: 2000,
+        easing: cubicInOut
+	});
+
+  let cameraZ = tweened(4, {
+		duration: 2000,
+        easing: cubicInOut
+	});
 </script>
 
 <Float rotationIntensity={0.4}>
@@ -15,15 +33,24 @@
   />
   <GLTF
     url="https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
-    interactive
-    on:click={() => {
-      console.log('User clicked!')
-    }}
     position={{ y: -1.2 }}
     useDraco
   >
-    <HTML transform distanceFactor={1.17} position={{ x: 0, y: 1.56, z: -1.4 }} rotation={{x: -0.256}}> 
-      <div class="htmlScreen">
+    <HTML transform distanceFactor={1.17} position={{ x: 0, y: 1.56, z: -1.4 }} rotation={{x: -0.256}}
+    >
+      <!-- add on:hover -->
+      <div class="htmlScreen" on:pointerenter={() => {
+        console.log('hover!')
+        $cameraX = 0;
+        $cameraY = 0;
+        $cameraZ = 2.8;
+      }}
+      on:pointerleave={() => {
+        console.log('User clicked!')
+        $cameraZ = 4;
+        $cameraY = 4;
+        $cameraX = -4;
+      }}>
         <iframe src="https://stefanreifenberg.com/portfolio/" />
       </div>
     </HTML>
@@ -43,7 +70,7 @@
  
 <Environment path="/hdr/" files="potsdamer_platz_1k.hdr" isBackground={false}/>
 
- <T.PerspectiveCamera makeDefault near={0.1} far={1000} zoom={1} position={[-4, 4, 4]}>
+ <T.PerspectiveCamera makeDefault near={0.1} far={1000} zoom={1} position={[$cameraX, $cameraY, $cameraZ]}>
      <OrbitControls 
           enableDamping
           maxPolarAngle={1}
@@ -52,7 +79,6 @@
           minAzimuthAngle={-0.75}
           enableZoom={false}
           target={{ y: 0.5 }}
-          
            />
  </T.PerspectiveCamera>
 
